ddd8a9a4efbf2e5cb964f0781103e91ddb523f92
Huang Lie Jun <liejun.hlj@gmail.com>
2017-10-08 15:30:56 +0800

    File structure

--- build_tagger.java
+++ build_tagger.java
@@ -1,11 +1,9 @@
-package com.hliejun;
-
 /**
  * Created by hliejun on 8/10/17.
  */
 public class build_tagger {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("BUILD COMMAND");
     }
 }
--- run_tagger.java
+++ run_tagger.java
@@ -1,12 +1,10 @@
-package com.hliejun;
-
 /**
  * Created by hliejun on 8/10/17.
  */
 public class run_tagger {
 
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("RUN COMMAND");
     }
 }
 
