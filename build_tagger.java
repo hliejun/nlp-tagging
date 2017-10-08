004ef32e1f34a7cd570a296e986fd705d2278cb0
Huang Lie Jun <liejun.hlj@gmail.com>
2017-10-08 16:00:16 +0800

    Revert back to structure

--- build_tagger.java
+++ build_tagger.java
@@ -1,6 +1,7 @@
 /**
  * Created by hliejun on 8/10/17.
  */
+
 public class build_tagger {
 
     public static void main(String[] args) {
--- run_tagger.java
+++ run_tagger.java
@@ -1,6 +1,7 @@
 /**
  * Created by hliejun on 8/10/17.
  */
+
 public class run_tagger {
 
     public static void main(String[] args) {
