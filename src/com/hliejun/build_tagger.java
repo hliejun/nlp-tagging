fd1006cceeb97775a358d93ff5b5c1cf90e7bba2
Huang Lie Jun <liejun.hlj@gmail.com>
2017-10-08 15:02:12 +0800

    Initialise project

---
+++ src/com/hliejun/build_tagger.java
@@ -0,0 +1,11 @@
+package com.hliejun;
+
+/**
+ * Created by hliejun on 8/10/17.
+ */
+public class build_tagger {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/hliejun/run_tagger.java
@@ -0,0 +1,12 @@
+package com.hliejun;
+
+/**
+ * Created by hliejun on 8/10/17.
+ */
+public class run_tagger {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
+
